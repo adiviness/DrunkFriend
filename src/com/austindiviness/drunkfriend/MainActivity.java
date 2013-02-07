c37a4c87b0f5753a5779295b18a070210c6afaba
Austin Diviness <adivines@mines.edu>
2013-02-06 21:15:50 -0700

    added alert dialogs for major control flow decisions, location manager should now end properly

--- src/com/austindiviness/drunkfriend/MainActivity.java
+++ src/com/austindiviness/drunkfriend/MainActivity.java
@@ -1,29 +1,30 @@
 package com.austindiviness.drunkfriend;
 
-import com.austindiviness.drunkfriend.R;
 import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.Collections;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.ContactsContract;
+import android.provider.Settings;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.TextView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -31,6 +32,7 @@ public class MainActivity extends Activity {
 	public LocationListener locationListener;
 	public LocationManager locationManager;
 	public String numberToText;
+	public String contactName;
 	public String message1 = "Hi, this is an automated message being sent to you because I'm drunk and would like to be picked up near the following location:";
 
 	@Override
@@ -48,54 +50,58 @@ public class MainActivity extends Activity {
 		listView.setAdapter(adapter);
 		// check if GPS is enabled
 		if (!gpsEnabled()) {
-			Toast.makeText(getBaseContext(), "Please enable GPS", Toast.LENGTH_LONG).show();
-			finish();
+			AlertDialog.Builder noGPSEnabled = new AlertDialog.Builder(MainActivity.this);
+			noGPSEnabled.setTitle("GPS Disabled");
+			noGPSEnabled.setMessage("Please enable GPS in settings menu");
+			noGPSEnabled.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {	
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					finish();
+				}
+			});
+			noGPSEnabled.setPositiveButton("GPS Settings", new DialogInterface.OnClickListener() {		
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO Auto-generated method stub
+					Intent gpsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+					startActivity(gpsIntent);
+					dialog.dismiss();
+				}
+			});
+			noGPSEnabled.show();
 		}
 		// set click listener
 		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				numberToText = "Invalid";
 				TextView textView = (TextView) view;
-				String name = textView.getText().toString();
-				Toast.makeText(getBaseContext(), name + " has been selected", Toast.LENGTH_SHORT).show();
-				// get number of selected contact
-				for(ContactData contact: data) {
-					if (contact.getName().equalsIgnoreCase(name)) {
-						numberToText = contact.getNumber();
-						break;
-					}
-				}
-				// check to make sure number matches correct contact, remove after done debugging
-				//Toast.makeText(getBaseContext(), numberToText, Toast.LENGTH_SHORT).show();
-				// get location
-				locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-				// I should check if the GPS is enabled here, however I'm leaving that for later
-				locationListener = new MyLocationListener();
-				String locationProvider = LocationManager.GPS_PROVIDER;
-				locationManager.requestLocationUpdates(locationProvider, 2000, 50, locationListener);
-				Toast.makeText(getBaseContext(), "Gathering GPS information, this will take a little while...", Toast.LENGTH_LONG).show();
-				// pause for 15 seconds, before running rest of code inside handler
-				Handler handler = new Handler();
-				handler.postDelayed(new Runnable() {
-					public void run() {
-						Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                        if (location == null) {
-                            Toast.makeText(getBaseContext(), "GPS either turned off or cannot contact satellites", Toast.LENGTH_LONG).show();
-                            finish();
-                        }
-						String lat = String.valueOf(location.getLatitude());
-						String lon = String.valueOf(location.getLongitude());
-						// correct order to display values is apparently lat, lon, even though it seems pretty ambiguous
-						//String message2 = "https://maps.google.com/maps?z=12&t=m&q=" + lon + "+N,+" + lat + "+W"; // Not the correct way to do is
-						String message2 = "https://maps.google.com/maps?z=12&t=m&q=" + lat + "," + lon; 
-						SmsManager sms = SmsManager.getDefault();
-						sms.sendTextMessage(numberToText, null, message1, null, null);
-						sms.sendTextMessage(numberToText, null, message2, null, null);
-						Toast.makeText(getBaseContext(), "Message sent!", Toast.LENGTH_SHORT).show();
-						finish();
-					}
-				}, 15000);
-				// end of handler code
+				contactName = textView.getText().toString();
+                // insert alert dialog here to make sure the user wants to text the user they picked
+				AlertDialog.Builder checkCorrectContact = new AlertDialog.Builder(MainActivity.this);
+                checkCorrectContact.setTitle("Confirm Contact");
+                checkCorrectContact.setMessage("Do you want to send pick up message to " + contactName + "?");
+                checkCorrectContact.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                	@Override
+                    public void onClick(DialogInterface dialog, int which) {
+                		// no, close the dialog
+                        dialog.dismiss();
+                    }
+                });
+                checkCorrectContact.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                	@Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // yes, send the message
+                		// get number of selected contact
+        				for(ContactData contact: data) {
+        					if (contact.getName().equalsIgnoreCase(contactName)) {
+        						numberToText = contact.getNumber();
+        						break;
+        					}
+        				}
+        				sendMessage(numberToText);
+                    }
+                });
+                checkCorrectContact.show();
 			}
 		});
 	}
@@ -124,6 +130,61 @@ public class MainActivity extends Activity {
 	public boolean gpsEnabled() {
 		LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 		return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+	}
+	
+	public void sendMessage(String contactNumber) {
+		// get location
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		locationListener = new MyLocationListener();
+		String locationProvider = LocationManager.GPS_PROVIDER;
+		locationManager.requestLocationUpdates(locationProvider, 2000, 50, locationListener);
+		Toast.makeText(getBaseContext(), "Gathering GPS information, this will take a little while...", Toast.LENGTH_LONG).show();
+		// pause for 15 seconds, before running rest of code inside handler
+		Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			public void run() {
+				Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location == null) {
+                    //Toast.makeText(getBaseContext(), "GPS either turned off or cannot contact satellites", Toast.LENGTH_LONG).show();
+                    AlertDialog.Builder badSignal = new AlertDialog.Builder(MainActivity.this);
+                    badSignal.setTitle("No Satellite Coverage");
+                    badSignal.setMessage("Insufficient satellite information is available. Perhaps try again outside?");
+                    badSignal.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                    	@Override
+                    	public void onClick(DialogInterface dialog, int which) {
+                    		// bad GPS signal, turn off location manager and exit
+                    		locationManager.removeUpdates(locationListener);
+                    		locationManager = null;
+                    		finish();
+                    	}
+                    });
+                    badSignal.show();
+                }
+                else {
+					String lat = String.valueOf(location.getLatitude());
+					String lon = String.valueOf(location.getLongitude());
+					// correct order to display values is apparently lat, lon, even though it seems pretty ambiguous
+					String message2 = "https://maps.google.com/maps?z=12&t=m&q=" + lat + "," + lon; 
+					SmsManager sms = SmsManager.getDefault();
+					sms.sendTextMessage(numberToText, null, message1, null, null);
+					sms.sendTextMessage(numberToText, null, message2, null, null);
+					AlertDialog.Builder messageSent = new AlertDialog.Builder(MainActivity.this);
+					messageSent.setTitle("Success!");
+					messageSent.setMessage("Successfully sent SMS message to " + contactName + " regarding your situation and present location.");
+					messageSent.setNeutralButton("OK", new DialogInterface.OnClickListener() {						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+			                // message sent, turn off location manager and exit
+			                locationManager.removeUpdates(locationListener);
+			                locationManager = null;
+							finish();	
+						}
+					});
+					messageSent.show();
+                }
+			}
+		}, 15000);
+		// end of handler code
 	}
 }
 
