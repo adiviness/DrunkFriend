5ca586d22176c28b2d2ef94dc715f1688fae0964
Austin Diviness <adivines@mines.edu>
2013-02-23 14:01:22 -0700

    added menu to FavoritesActivity, buttons now reload text in case user changes options from selecting settings in menu at FavoritesActivity

--- src/com/austindiviness/drunkfriend/FavoritesActivity.java
+++ src/com/austindiviness/drunkfriend/FavoritesActivity.java
@@ -1,14 +1,19 @@
 package com.austindiviness.drunkfriend;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class FavoritesActivity extends Activity {
+	public int mainMenuId = 1;
+	public int settingsId = Menu.FIRST;
 	private int[] buttonIds = {
 			R.id.speed_dial_1,
 			R.id.speed_dial_2,
@@ -31,6 +36,11 @@ public class FavoritesActivity extends Activity {
 	protected void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		setContentView(R.layout.favorites_layout);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(FavoritesActivity.this);
 		for (int i = 0; i < buttonIds.length; ++i) {
 			int buttonId = buttonIds[i];
@@ -38,13 +48,28 @@ public class FavoritesActivity extends Activity {
 			Button tempButton = (Button) findViewById(buttonId);
 			tempButton.setText(prefs.getString(buttonPref, "null"));
 		}
-		
 	}
 	
 	public void favoriteButtonClick(View v) {
 		Button button = (Button) v;
 		CharSequence which =  button.getText();
 		Toast.makeText(getBaseContext(), which, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(mainMenuId, settingsId, 0, "Settings");
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+			case Menu.FIRST:
+				Intent settingsIntent = new Intent(FavoritesActivity.this, SettingsActivity.class);
+				FavoritesActivity.this.startActivity(settingsIntent);
+		}
+		return true;
 	}
 
 }
