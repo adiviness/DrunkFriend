00a5b5ca88cf1373d483b4603628f2acfa966f30
Austin Diviness <adivines@mines.edu>
2013-02-23 13:28:36 -0700

    mapped speed dial buttons to preferences in setting menu

--- src/com/austindiviness/drunkfriend/FavoritesActivity.java
+++ src/com/austindiviness/drunkfriend/FavoritesActivity.java
@@ -1,18 +1,44 @@
 package com.austindiviness.drunkfriend;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class FavoritesActivity extends Activity {
-	boolean[] buttonSet = {false, false, false, false, false, false};
+	private int[] buttonIds = {
+			R.id.speed_dial_1,
+			R.id.speed_dial_2,
+			R.id.speed_dial_3,
+			R.id.speed_dial_4,
+			R.id.speed_dial_5,
+			R.id.speed_dial_6
+	};
+	
+	private String[] buttonPrefNames = {
+		"dial1",
+		"dial2",
+		"dial3",
+		"dial4",
+		"dial5",
+		"dial6"
+	};
 	
 	@Override
 	protected void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		setContentView(R.layout.favorites_layout);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(FavoritesActivity.this);
+		for (int i = 0; i < buttonIds.length; ++i) {
+			int buttonId = buttonIds[i];
+			String buttonPref = buttonPrefNames[i];
+			Button tempButton = (Button) findViewById(buttonId);
+			tempButton.setText(prefs.getString(buttonPref, "null"));
+		}
+		
 	}
 	
 	public void favoriteButtonClick(View v) {
--- src/com/austindiviness/drunkfriend/SettingsActivity.java
+++ src/com/austindiviness/drunkfriend/SettingsActivity.java
@@ -1,13 +1,32 @@
 package com.austindiviness.drunkfriend;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity {
-
+		String[] temp = {"hello", "world"};
+		String[] tempValues = {"0", "1"};
+		
+		private String[] buttonPrefNames = {
+			"dial1",
+			"dial2",
+			"dial3",
+			"dial4",
+			"dial5",
+			"dial6"
+		};
+		private ListPreference listPref;
+		
 		@Override
 		protected void onCreate(Bundle bundle) {
 			super.onCreate(bundle);
 			addPreferencesFromResource(R.layout.settings);
+			
+			for (String buttonPrefName: buttonPrefNames) {
+				listPref = (ListPreference) findPreference(buttonPrefName);
+				listPref.setEntries(temp);
+				listPref.setEntryValues(tempValues);
+			}
 		}
 }
